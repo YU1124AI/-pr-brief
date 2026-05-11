48a51f83822d0810c565f8f93de79d5b1dab11ae
YU1124AI <1124ys.aihara@gmail.com>
2026-05-12 01:32:46 +0900

    Add files via upload

--- dentsu-pr-brief/next.config.js
+++ dentsu-pr-brief/next.config.js
@@ -4,15 +4,19 @@ const nextConfig = {
     remotePatterns: [
       { protocol: 'https', hostname: 'prcdn.freetls.fastly.net' },
       { protocol: 'https', hostname: '**.prtimes.jp' },
+      { protocol: 'https', hostname: 'prtimes.jp' },
       { protocol: 'https', hostname: 'predge.jp' },
-      { protocol: 'https', hostname: '**.wp.com' },
-      { protocol: 'https', hostname: 'i.imgur.com' },
-      { protocol: 'https', hostname: '**.ytimg.com' },
+      { protocol: 'https', hostname: '**.predge.jp' },
       { protocol: 'https', hostname: 's.yimg.jp' },
       { protocol: 'https', hostname: '**.yimg.jp' },
       { protocol: 'https', hostname: '**.yahoo.co.jp' },
+      { protocol: 'https', hostname: 'www.asahi.com' },
+      { protocol: 'https', hostname: '**.asahi.com' },
+      { protocol: 'https', hostname: 'www.sendenkaigi.com' },
+      { protocol: 'https', hostname: '**.sendenkaigi.com' },
+      { protocol: 'https', hostname: '**.wp.com' },
+      { protocol: 'https', hostname: 'api.allorigins.win' },
     ],
   },
-};
-
-module.exports = nextConfig;
+}
+module.exports = nextConfig
